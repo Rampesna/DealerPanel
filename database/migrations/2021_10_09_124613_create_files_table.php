2f598472f63a1fb8ddf4d83067399698650a59a6
Talha Can <rampesna@gmail.com>
2021-10-09 15:52:50 +0300

    v0.4.8.88

---
+++ database/migrations/2021_10_09_124613_create_files_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('files', function (Blueprint $table) {
+            $table->id();
+            $table->string('creator_type');
+            $table->bigInteger('creator_id')->unsigned();
+            $table->string('relation_type');
+            $table->bigInteger('relation_id')->unsigned();
+            $table->text('path');
+            $table->string('mimetype')->nullable();
+            $table->string('icon')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('files');
+    }
+}
